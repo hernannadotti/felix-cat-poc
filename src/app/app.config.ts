a9f461dd9eda4d766edb00ae7f48a8a52f8b32f7
hernan@ardentcreative.com <hernan@mobomo.com>
2024-02-05 09:45:32 -0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { metaReducers, reducers } from './store/reducers';
+import { PetsEffects } from './store/effects/pets.effects';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideStore(reducers, { metaReducers }),
+    provideStoreDevtools(),
+    provideEffects([PetsEffects])]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PetListComponent } from './pet-list/pet-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'pets',
+    component: PetListComponent
+  }
+];
